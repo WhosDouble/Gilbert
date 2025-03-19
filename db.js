a9f201b3ba1c5003c9f465b355b930a15d6de978
SirRango <sturdivantdevon78@gmail.com>
2025-03-19 10:44:42 -0500

    changed db again

--- db.js
+++ db.js
@@ -1,18 +1,13 @@
-import pkg from "pg";
-
-const { Client } = pkg;
-
+import { Client } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env file
 
-// Create a new client instance
 const db = new Client({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Allow SSL connections
+  },
 });
 
 db.connect()
