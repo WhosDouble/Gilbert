963ac427154b5412e12feed0568e0f64f297525b
SirRango <sturdivantdevon78@gmail.com>
2025-03-19 22:35:40 -0500

    again

--- index.js
+++ index.js
@@ -421,6 +421,8 @@ client.on("messageCreate", async (message) => {
 
     // Check if the user has a stored message
     if (!pastMessages.has(message.author.id)) {
+      pastMessages = [];
+    } else {
       pastMessages = lastMessages.get(message.author.id);
     }
 
